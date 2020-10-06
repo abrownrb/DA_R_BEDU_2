9336ea60d5bf611cd758b4d1f5ec4e71be349fc4
abrownrb <alicia.brownrb@gmail.com>
2020-10-06 08:25:30 -0500

    Add files via upload

---
+++ Sesion-01/intro.R
@@ -0,0 +1,16 @@
+ejemplo_suma <- 2+3 # tomo dos valores, los sumo y los guardo en la variable llamada ejemplo_suma
+ejemplo_multiplicacion <- 3*5
+
+ejemplo_multiplicacion - ejemplo_suma
+
+
+nombre <- "Alicia"
+
+install.packages('dplyr') 
+
+install.packages("dplyr") 
+library(dplyr)
+
+base::mean(c(2,3))
+sum(2,3, NA, na.rm = FALSE) # no ignoro los NA
+sum(2,3, NA, na.rm = TRUE) # ignorando los NA
